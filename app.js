065840ae290a0ce08fd1e0834ed47c4fb454aa92
Shalitha Suranga <shalithasuranga@gmail.com>
2017-11-11 16:14:17 +0530

    Change config

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const userController = require('./controllers/userController');
 
 
-
+var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
-
+app.use(express.static(__dirname));
 app.get('/', (req, res) => userController(req, res));
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('app is listening to port 3000.');
 });
